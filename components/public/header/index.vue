546aa7f7b258538819094bc3e6d134256c2e5b5d
power <1410837981@qq.com>
2019-04-08 16:02:56 +0800

    Header-首页引导导航功能

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,27 +1,30 @@
 <template>
-  <el-row :gutter="15">
-    <el-col :span="6">
+  <el-row :gutter="0" class="m-header">
+    <el-col :span="4">
       <geo></geo>
     </el-col>
     <el-col :span="6">
       <user></user>
     </el-col>
-    <el-col :span="6"></el-col>
+    <el-col :span="14">
+      <navbar></navbar>
+    </el-col>
   </el-row>
 </template>
 
 <script>
   import Geo from './geo'
   import User from './user'
+  import Navbar from './nav'
     export default {
         components:{
           Geo,
-          User
+          User,
+          Navbar
         }
     }
 </script>
 
 <style lang="scss">
-  @import "@/assets/css/public/layout.scss";
-
+  @import "@/assets/css/public/header/index.scss";
 </style>
